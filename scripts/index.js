0c9014b426b5da7878997e68b848cc13aeb7d0eb
sergbfmv <71472837+sergbfmv@users.noreply.github.com>
2021-04-13 22:45:34 +0300

    popup photo added

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+const popupAvatar = document.querySelector('.popup_type_avatar')
+const closeButtonAvatar = document.querySelector('.popup__close-button_type_avatar')
+const avatar = document.querySelector('.avatar')
+const popups = document.querySelectorAll('.popup')
+
+function closeByEsc(evt) {
+  if (evt.key === 'Escape') {
+    closePopup(document.querySelector('.popup_opened'))
+  }
+}
+
+function openPopup(popup) {
+  popup.classList.add('popup_opened')
+  document.addEventListener('keydown', closeByEsc)
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened')
+}
+
+popups.forEach((popup) => {
+  popup.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('popup_opened')) {
+      closePopup(popup)
+    }
+    if (evt.target.classList.contains('popup__close-button')) [
+      closePopup(popup)
+    ]
+  })
+})
+
+avatar.addEventListener('click', function() {
+  openPopup(popupAvatar)
+})
